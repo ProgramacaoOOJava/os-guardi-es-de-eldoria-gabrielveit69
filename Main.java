45f473241b3cbbe64617cea4ed7883eb1b91cf01
Erico Gabriel Veit Ferreira <e.gabrielveit@live.com>
2026-07-22 21:46:17 +0000

    desafio iniciante

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
+package Eudorianovato;
+
 public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+	public static void main(String[] args) {
+		Personagem personagem1 = new Personagem("Diego", "Bárbaro", 1, 15, 2.55);
+		Personagem personagem2 = new Personagem("Emerson", "Bardo", 1, 8, 2.55);
+		personagem1.exibirStatus();
+		personagem2.exibirStatus();
+	}
 
-        .
-        .
-        .
-
-    }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,24 @@
+package Eudorianovato;
+
 public class Personagem {
-    String nome;
-    String classe;
-    int nivel;
-    int pontosDeVida;
-    double poderBase;
+	String nome;
+	String classe;
+	int nivel;
+	int pv;
+	double power;
+	
+	public Personagem(String nome, String classe, int nivel, int pv, double power) {
+		this.nome = nome;
+		this.classe = classe;
+		this.nivel = nivel;
+		this.pv = pv;
+		this.power = power;
+		
+	}
+	
+	public void exibirStatus() {
+		System.out.println("Nome: " + nome + "\n" + "Classe: " + classe + "\n" + "Nível: " + nivel + "\n" + "Pontos de Vida: " + pv + "\n" + "Poder Base: " + power + "\n\n");
+	}
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
-        this.nome = nome;
-        .
-        .
-        .
-
-    }
-
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
-    }
 }
 
